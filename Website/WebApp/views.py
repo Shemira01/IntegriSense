6672076b20898e573a030d4edecf37660d4881dc
Shemira01 <145838195+Shemira01@users.noreply.github.com>
2025-06-10 13:01:42 +0300

    Django done

--- Website/WebApp/views.py
+++ Website/WebApp/views.py
@@ -1,3 +1,5 @@
+# WebApp/views.py
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'WebApp/index.html')
--- Website/settings.py
+++ Website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'WebApp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -116,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'WebApp/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
